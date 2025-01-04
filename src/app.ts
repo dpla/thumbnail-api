1ac9796888846dcf7cff5a34f0e9e35fc1f1d7e4
Michael Della Bitta <mdellabitta@tutanota.com>
2025-01-04 16:28:49 -0500

    logging + handlers + cluster fixes
    - switched to winston for logging
    - added morgan for access logging
    - made /health never 304
    - uncaughtException handler
    - unhandledRejection handler
    - respawning crashed worker fix

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 import "./instrumentation.js"; //IMPORTANT: keep this first
+import morgan from "morgan";
 import * as Sentry from "@sentry/node";
 import express, { Express } from "express";
 import { S3Client } from "@aws-sdk/client-s3";
 import { SQSClient } from "@aws-sdk/client-sqs";
 import { ThumbnailApi } from "./ThumbnailApi.js";
 import { default as cluster, Worker } from "node:cluster";
-import { cpus } from "node:os";
+import { availableParallelism } from "node:os";
 import { DplaApi } from "./DplaApi";
 import { ThumbnailStorage } from "./ThumbnailStorage";
 import { ThumbnailCacheQueue } from "./ThumbnailCacheQueue";
 import { ResponseHelper } from "./ResponseHelper";
+import { getLogger } from "./logger";
 
+const logger = getLogger();
 // How long we wait for a request from a socket
 const REQUEST_TIMEOUT = 3000; // 3 secs
 
@@ -20,7 +23,7 @@ const RESPONSE_TIMEOUT = 10000; // 10 seconds
 start();
 
 function start() {
-  const numCPUs = Number(process.env.PS_COUNT) || cpus().length;
+  const numCPUs = Number(process.env.PS_COUNT) || availableParallelism();
   const mustFork =
     process.env.MUST_FORK === "true" || process.env.NODE_ENV === "production";
 
@@ -47,6 +50,13 @@ function doWorker() {
   }
 
   const app: Express = express();
+  app.use(
+    morgan(":method :url :status :res[content-length] - :response-time ms", {
+      stream: {
+        write: (message: string) => logger.info(message.trim()),
+      },
+    }),
+  );
   const s3Client = new S3Client(awsOptions);
   const sqsClient = new SQSClient(awsOptions);
 
@@ -72,7 +82,7 @@ function doWorker() {
     try {
       await thumbnailApi.handle(req, res);
     } catch (error) {
-      console.error("Error fetching thumb:", error);
+      logger.error("Unexpected Error fetching thumb:", error);
     }
   };
 
@@ -80,25 +90,30 @@ function doWorker() {
     handler(req, res)
       .then(() => Promise.resolve())
       .catch((reason: unknown) => {
-        console.error("Caught error from handler: %s", reason);
+        logger.error("Caught error from handler: %s", reason);
       });
   });
 
   app.get("/health", (_req: express.Request, res: express.Response): void => {
-    res.sendStatus(200).end();
+    const healthcheck = {
+      message: "OK",
+      uptime: process.uptime(),
+      timestamp: Date.now(),
+    };
+    res.status(200).send(healthcheck).end();
   });
 
   Sentry.setupExpressErrorHandler(app);
 
   const server = app.listen(port, (): void => {
-    console.log(`Server is listening on ${String(port)}`);
+    logger.info(`Server is listening on ${String(port)}`);
   });
 
   // how long the server waits before the client needs to finish sending the request
   server.requestTimeout = REQUEST_TIMEOUT;
 
   const handleExit: NodeJS.SignalsListener = (signal: NodeJS.Signals) => {
-    console.log("Received %s. Shutting down.", signal);
+    logger.info("Received %s. Shutting down.", signal);
     server.close(() => {
       process.exit(0);
     });
@@ -107,21 +122,35 @@ function doWorker() {
   process.on("SIGINT", () => {
     handleExit("SIGINT");
   });
+
   process.on("SIGQUIT", () => {
     handleExit("SIGQUIT");
   });
+
   process.on("SIGTERM", () => {
     handleExit("SIGTERM");
+  });
+
+  process.on("uncaughtException", (err: unknown) => {
+    logger.error("Uncaught exception. Shutting down.", err);
+    process.exit(1);
+  });
+
+  process.on("unhandledRejection", (err) => {
+    logger.error(err);
+    process.exit(1);
   });
 }
 
 function doFork(numCPUs: number): void {
   cluster
     .on("exit", (worker: Worker): void => {
-      console.log(`worker ${String(worker.process.pid)} died`);
+      logger.info(`Worker ${String(worker.process.pid)} died`);
+      logger.info("Initiating replacement worker.");
+      cluster.fork();
     })
     .on("online", (worker: Worker): void => {
-      console.log(`worker ${String(worker.process.pid)} online`);
+      logger.info(`worker ${String(worker.process.pid)} online`);
     });
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
